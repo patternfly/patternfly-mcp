6634b01c62095910d01932b34961a5470b8dd5bc
CD Cabrera <ccabrera@redhat.com>
2025-12-19 13:11:30 -0500

    refactor: options, nodejs version, path resolves (#58)

--- src/__tests__/options.defaults.test.ts
+++ src/__tests__/options.defaults.test.ts
@@ -1,7 +1,31 @@
-import * as options from '../options.defaults';
+import { DEFAULT_OPTIONS, getNodeMajorVersion } from '../options.defaults';
 
 describe('options defaults', () => {
   it('should return specific properties', () => {
-    expect(options).toMatchSnapshot();
+    expect(DEFAULT_OPTIONS).toMatchSnapshot('defaults');
+  });
+});
+
+describe('getNodeMajorVersion', () => {
+  it('should get the current Node.js version', () => {
+    // Purposeful failure in the event the process.versions.node value is not available
+    expect(getNodeMajorVersion()).not.toBe(0);
+  });
+
+  it.each([
+    {
+      description: 'number',
+      value: 1_000_000
+    },
+    {
+      description: 'string',
+      value: 'lorem ipsum'
+    },
+    {
+      description: 'null',
+      value: null
+    }
+  ])('should handle basic failure, $description', ({ value }) => {
+    expect(getNodeMajorVersion(value as any)).toBe(0);
   });
 });
