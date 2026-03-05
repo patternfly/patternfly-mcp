c04aa0dcb9e0375326f697844abd03d0a526a28b
CD Cabrera <ccabrera@redhat.com>
2026-03-05 11:04:05 -0500

    chore(docs): add ai, extensions, foundations, styles guidance (#121)
    * testing, metadata checks
    * update version refs for org links, align org links
    * adds for ai, extensions, foundations, styles, ouia, and includes design tokens

--- src/__tests__/docs.json.test.ts
+++ src/__tests__/docs.json.test.ts
@@ -1,20 +1,49 @@
 import docs from '../docs.json';
 
 describe('docs.json', () => {
-  it('should have metadata reflective of its JSON content', () => {
-    expect(docs.meta.totalEntries).toBeDefined();
-    expect(Object.entries(docs.docs).length).toBe(docs.meta.totalEntries);
-
-    expect(docs.meta.totalDocs).toBeDefined();
-
+  it('should have metadata reflective of its content and unique links per each entry', () => {
+    const allLinks = new Set<string>();
+    const baseHashes = new Set<string | undefined>();
+    const flatDocs = Object.values(docs.docs).flat();
     let totalDocs = 0;
 
-    Object.values(docs.docs).forEach(entries => {
-      if (Array.isArray(entries)) {
-        totalDocs += entries.length;
+    flatDocs.forEach(entry => {
+      totalDocs += 1;
+      allLinks.add(entry.path);
+
+      if (entry.path.includes('documentation:')) {
+        baseHashes.add('documentation:');
+      } else if (entry.path.includes('/patternfly/patternfly-org/')) {
+        baseHashes.add(entry.path.split('/patternfly/patternfly-org/')[1]?.split('/')[0]);
+      } else if (entry.path.includes('/patternfly/patternfly-react/')) {
+        baseHashes.add(entry.path.split('/patternfly/patternfly-react/')[1]?.split('/')[0]);
+      } else {
+        baseHashes.add(`new-resource-${entry.path}`);
       }
     });
 
+    expect(docs.meta.totalEntries).toBeDefined();
+    expect(docs.meta.totalDocs).toBeDefined();
+    expect(Object.entries(docs.docs).length).toBe(docs.meta.totalEntries);
+
+    /**
+     * Confirm we have limited hashes, avoid variation within pf versions
+     * If this increases, hashes need to be realigned. Do not randomly change this value.
+     * 1 (v6 org) + 1 (v6 react) + 1 (v5 org) + 1 (local)
+     */
+    expect(baseHashes.size).toBe(4);
+
+    /**
+     * Confirm total docs count matches metadata
+     * Update the JSON metadata accordingly
+     */
     expect(totalDocs).toBe(docs.meta.totalDocs);
+
+    /**
+     * Confirm unique links against metadata totals
+     * Update the JSON metadata accordingly
+     */
+    expect(allLinks.size).toBe(flatDocs.length);
+    expect(allLinks.size).toBe(docs.meta.totalDocs);
   });
 });
