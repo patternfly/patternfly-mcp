47e54e36d06d4158fa7460e96507ae05fae83976
CD Cabrera <cabrera.code@gmail.com>
2026-05-16 22:12:32 -0400

    refactor(server): pf-3836 colocate register tools, resources
    * server, colocate registering tools, resources
    * options.registry, centralize server imports for tools, resources

---
+++ src/options.registry.ts
@@ -0,0 +1,35 @@
+import { type McpToolCreator, type McpResourceCreator } from './server';
+import { usePatternFlyDocsTool } from './tool.patternFlyDocs';
+import { searchPatternFlyDocsTool } from './tool.searchPatternFlyDocs';
+import { patternFlyComponentsIndexResource } from './resource.patternFlyComponentsIndex';
+import { patternFlyContextResource } from './resource.patternFlyContext';
+import { patternFlyDocsIndexResource } from './resource.patternFlyDocsIndex';
+import { patternFlyDocsTemplateResource } from './resource.patternFlyDocsTemplate';
+import { patternFlySchemasIndexResource } from './resource.patternFlySchemasIndex';
+import { patternFlySchemasTemplateResource } from './resource.patternFlySchemasTemplate';
+
+/**
+ * Built-in tools.
+ *
+ * Array of built-in tools
+ */
+const builtinTools: McpToolCreator[] = [
+  usePatternFlyDocsTool,
+  searchPatternFlyDocsTool
+];
+
+/**
+ * Built-in resources.
+ *
+ * Array of built-in resources
+ */
+const builtinResources: McpResourceCreator[] = [
+  patternFlyContextResource,
+  patternFlyComponentsIndexResource,
+  patternFlyDocsIndexResource,
+  patternFlyDocsTemplateResource,
+  patternFlySchemasIndexResource,
+  patternFlySchemasTemplateResource
+];
+
+export { builtinTools, builtinResources };
