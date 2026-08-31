0d2efaf5aa45ba099c728dea9d2be41e071cd7e3
CD Cabrera <ccabrera@redhat.com>
2026-08-31 10:15:31 -0400

    refactor: pf-4402 add tool, resource provenance (#292)
    * tool.patternFlyDocs, align to resource provenance 
    * resource.patternFlyDocsTemplate, provenance, avoid broadcasting paths that cause noise

--- src/resource.patternFlyDocsTemplate.ts
+++ src/resource.patternFlyDocsTemplate.ts
@@ -118,7 +118,8 @@ const resourceCallback = async (passedUri: URL, variables: Record<string, string
   try {
     const docPaths = byEntry
       .filter(({ path }) => path)
-      .map(({ path, uriId }) => ({ doc: path, uri: uriId }));
+      .map(({ path, uriId, id, groupId, displayName, displayCategory, version: entryVersion }) =>
+        ({ doc: path, uri: uriId, id, groupId, displayName, displayCategory, entryVersion }));
 
     if (docPaths.length > 0) {
       // `processDocsFunction` has de-dup docs baked in
@@ -172,11 +173,12 @@ const resourceCallback = async (passedUri: URL, variables: Record<string, string
   }
 
   return {
-    contents: docs.map(({ uri, path, resolvedPath, content }) => ({
+    contents: docs.map(({ uri, content, id, groupId, displayName, displayCategory, entryVersion }) => ({
       uri,
       mimeType: 'text/markdown',
       text: stringJoin.newline(
-        `# Documentation from ${resolvedPath || path}`,
+        `<!-- mcp:provenance id="${id}" groupId="${groupId}" -->`,
+        `# Documentation for ${displayName} - ${displayCategory} (${entryVersion})`,
         '',
         content
       )
